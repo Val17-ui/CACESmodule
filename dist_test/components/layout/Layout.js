24809937a69ec2e9f1eef374fb7f12f2b8e8437e
Val17-ui <vanhammevalerie@gmail.com>
2025-07-09 14:57:04 +0200

    effacer test

--- dist_test/components/layout/Layout.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Sidebar from './Sidebar';
-import Header from './Header';
-const Layout = ({ children, title, subtitle, actions, activePage, onPageChange, }) => {
-    return (_jsxs("div", { className: "flex h-screen bg-gray-50", children: [_jsx(Sidebar, { activePage: activePage, onPageChange: onPageChange }), _jsxs("div", { className: "flex-1 flex flex-col overflow-hidden", children: [_jsx(Header, { title: title, subtitle: subtitle, actions: actions }), _jsx("main", { className: "flex-1 overflow-y-auto p-6", children: children })] })] }));
-};
-export default Layout;
